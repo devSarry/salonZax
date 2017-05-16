7cfca45cad77e1339b0838dbae8282c450356289
Joanthan Sarry <jsarry@gmail.com>
2017-05-16 20:24:32 +0300

    optional banner image

--- database/seeds/ContactSectionSeeder.php
+++ database/seeds/ContactSectionSeeder.php
@@ -11,7 +11,7 @@ class ContactSectionSeeder extends Seeder {
      */
     public function run()
     {
-        $contact = new \App\ContactSection();
+
         $data = [
             'formated_address' => 'Kontiontie 14, 53300 Lappeenranta, Finland',
             'wrapped_address'  => '<span class="street-address">Kontiontie 14</span>, <span class="postal-code">53300</span> <span class="locality">Lappeenranta</span>, <span class="country-name">Finland</span>',
@@ -30,15 +30,20 @@ class ContactSectionSeeder extends Seeder {
             'twitter_url'      => 'www.twitter.com',
         ];
 
-
+        $contact = new \App\ContactSection();
         $contact->fill($data);
         $contact->save();
-
         $contact->section()->create([
             'title' => 'QUESTIONS OR COMMENTS?',
             'body'  => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi aut delectus dolorum eius enim explicabo hic illum non quam quasi ratione saepe sapiente similique tenetur veniam vitae, voluptas! Omnis?',
             'color' => 'style2',
         ]);
+
+        $contact = new \App\ContactSection();
+        $contact->fill($data);
+        $contact->save();
+
+
     }
 
 }
